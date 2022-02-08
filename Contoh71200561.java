47c451d24379b61e3a9312224af76558ed6d08d4
valensiafernanda <78332934+valensiafernanda@users.noreply.github.com>
2022-02-08 15:57:02 +0700

    Create Contoh71200561.java

---
+++ Contoh71200561.java
@@ -0,0 +1,14 @@
+		public class Contoh 71200561 {
+			public static void main(String args) {
+				int x=10;
+				int y=25;
+				int z=x+y;
+				
+				System.out.println("Hasil penjumlahan" + " dari x+y = " + z;
+				System.out.println("Hasil akar kuadrat " + "dari z =" + Math.sqrt(z));
+				
+				String namadepan = "Antonius";
+				System.out.println("Nama depan saya adalah = " + namadepan);
+				System.out.println("Nama lengkap saya " +"adalah = " +nama depan + " Rachmat");
+		}
+	}
